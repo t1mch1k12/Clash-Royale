3d5468e5e419a4779b321b9c49b6e60303c702a8
82v-82 <vadimdan100@gmail.com>
2026-03-23 03:20:17 -1100

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,1 +1,27 @@
-
+window.drawTowerScore = function(ctx, playerTowers, enemyTowers) {
+    // Свои башни (синие)
+    for (let i = 0; i < playerTowers; i++) {
+        ctx.fillStyle = '#3a6ea5';
+        ctx.fillRect(20 + i * 30, 80, 20, 40);
+    }
+    
+    // Оставшиеся (серые)
+    for (let i = playerTowers; i < 3; i++) {
+        ctx.fillStyle = '#666666';
+        ctx.fillRect(20 + i * 30, 80, 20, 40);
+    }
+    
+    // Башни врага (красные справа)
+    for (let i = 0; i < enemyTowers; i++) {
+        ctx.fillStyle = '#a53a3a';
+        ctx.fillRect(700 - i * 30, 80, 20, 40);
+    }
+    
+    // Разделительная линия
+    ctx.strokeStyle = '#ffd700';
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(400, 50);
+    ctx.lineTo(400, 120);
+    ctx.stroke();
+}
