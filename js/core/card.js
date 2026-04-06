792e4e908eb1606712013c390d213a6b3ea7d96c
lafneroo <ostapcukandrej41@gmail.com>
2026-04-06 16:44:37 +0300

    Update card.js

--- js/core/card.js
+++ js/core/card.js
@@ -1,1 +1,41 @@
+// ============================================================
+// card.js - Класс карты
+// ============================================================
 
+class Card {
+    constructor(cardId, cardData) {
+        this.id = cardId;
+        this.name = cardData.name;
+        this.cost = cardData.cost;
+        this.unitType = cardData.unitType;
+        this.icon = cardData.icon || cardData.unitType;
+        this.description = cardData.description || '';
+        this.rarity = cardData.rarity || 'common';
+        
+        // Характеристики юнита
+        this.hp = cardData.hp;
+        this.damage = cardData.damage;
+        this.range = cardData.range;
+        this.speed = cardData.speed;
+        this.attackSpeed = cardData.attackSpeed;
+    }
+    
+    getTooltip() {
+        return `${this.name} (${this.cost}⚡)\n❤️ ${this.hp} | ⚔️ ${this.damage}`;
+    }
+    
+    clone() {
+        return new Card(this.id, {
+            name: this.name,
+            cost: this.cost,
+            unitType: this.unitType,
+            hp: this.hp,
+            damage: this.damage,
+            range: this.range,
+            speed: this.speed,
+            attackSpeed: this.attackSpeed
+        });
+    }
+}
+
+window.Card = null;
