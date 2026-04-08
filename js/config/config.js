082adc115afbbc9804dd3833db29f7d923441523
t1mch1k12 <kim228455@gmail.com>
2026-04-08 18:51:16 +0300

    Update config.js

--- js/config/config.js
+++ js/config/config.js
@@ -4,6 +4,7 @@
 
 window.CONFIG = {
     IMAGES: {
+        // Арена и ландшафт
         path: 'assets/images/arena/path.png',
         grass: 'assets/images/arena/grass.png',
         river: 'assets/images/arena/river.png',
@@ -16,6 +17,7 @@ window.CONFIG = {
     },
     
     SOUNDS: {
+        // Игровые эффекты
         deploy: 'assets/sounds/deploy.mp3',
         hit: 'assets/sounds/hit.mp3',
         towerHit: 'assets/sounds/tower_hit.mp3',
@@ -24,6 +26,7 @@ window.CONFIG = {
     },
     
     GAME: {
+        // Игра
         width: 900,
         height: 600,
         maxElixir: 10,
@@ -31,6 +34,7 @@ window.CONFIG = {
         elixirRegenRate: 2.8, // секунды на 1 эликсир
         
         towers: {
+            // Башни
             playerLeft: { x: 150, y: 450, maxHp: 1500, damage: 50, range: 100 },
             playerRight: { x: 750, y: 450, maxHp: 1500, damage: 50, range: 100 },
             playerKing: { x: 450, y: 500, maxHp: 3000, damage: 70, range: 120 },
@@ -41,7 +45,9 @@ window.CONFIG = {
     },
     
     CARDS: {
+        // Карты
         knight: {
+            // Рыцарь
             name: 'Рыцарь',
             cost: 3,
             unitType: 'knight',
@@ -54,6 +60,7 @@ window.CONFIG = {
             rarity: 'common'
         },
         archer: {
+            // Лучник
             name: 'Лучница',
             cost: 3,
             unitType: 'archer',
@@ -66,6 +73,7 @@ window.CONFIG = {
             rarity: 'common'
         },
         mage: {
+            // Маг
             name: 'Маг',
             cost: 4,
             unitType: 'mage',
