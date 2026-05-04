56996def373c8e5b438e47ac888ec49c4a0925fa
t1mch1k12 <kim228455@gmail.com>
2026-05-04 17:15:21 +0300

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,13 @@
+const player = document.getElementById('player');
 
+let playerX = 0;
+const speed = 5; // скорость движения
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowRight') {
+    playerX += speed;
+  } else if (event.key === 'ArrowLeft') {
+    playerX -= speed;
+  }
+  player.style.left = playerX + 'px';
+});
